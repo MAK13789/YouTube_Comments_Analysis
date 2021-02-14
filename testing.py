2899cff826817b9484864a1104b3234757b02b92
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-02-14 20:49:10 +0500

    :sunglasses:

---
+++ testing.py
@@ -0,0 +1,9 @@
+import csv
+data_list = []
+with open('query_data.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        data_list.append(row)
+#print (data_list)
+data = [item for sublist in data_list for item in sublist]
+print (data)
