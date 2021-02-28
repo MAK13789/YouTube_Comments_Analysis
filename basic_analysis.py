aa2b4b9a0d60e32806c564ab14e3508b9282f71d
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-02-28 20:24:42 +0500

    :winking_face:

--- basic_analysis.py
+++ basic_analysis.py
@@ -2,6 +2,9 @@
 in this file i will be experimenting with some basic analysis techniques (i do not have experience in this stuff before so this is simply for learning purposes)
 '''
 import pandas as pd 
+from nltk.corpus import stopwords
+from textblob import TextBlob
+from tqdm import tqdm
 dataset = pd.read_csv('dataset.csv')
 #basic pre-processing functions:
 def lower_case(data):
@@ -16,7 +19,6 @@ def remove_punctuation(data):
 
 def remove_stopwords(data):
     '''removes unnecessary stopwords, might not be needed for this project though'''
-    from nltk.corpus import stopwords
     stop = stopwords.words('english')
     data['Comment'] = data['Comment'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
 
@@ -33,5 +35,18 @@ def remove_most_rare_words(data):
     freq = pd.Series(' '.join(data['Comment']).split()).value_counts()[-10:]
     freq = list(freq.index)
     data['Comment'] = data['Comment'].apply(lambda x: " ".join(x for x in x.split() if x not in freq))
+
+
+def spelling_correction(data):
+    '''corrects spelling of comments, is slow and may or may not be accurate, YEA I THINK BETTER NOT TO USE IT'''
+    data['Comment'].apply(lambda x: str(TextBlob(x).correct()))
+
+
+def tokenize(data):
+    '''tokenizes comments: divides them into lists of words, and adds the result to a new column called Tokenized Comment'''
+    all_tokenized = []
+    for i in tqdm(range(len(data.index)), ncols=100):
+        all_tokenized.append(list(TextBlob(data['Comment'][i]).words))
+    data['Tokenized Comment'] = all_tokenized
 
 
