970c76891a1188a357ceb57da46ec2da1b7ba213
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-12-30 18:13:52 -0500

    :eye:

--- plain_sentiment.py
+++ plain_sentiment.py
@@ -7,7 +7,19 @@ from tqdm import tqdm
 dataset = pd.read_csv('dataset.csv')
 def plain_sentiment(data):
     '''takes in a dataset in the form of dataset.csv and for each comment adds the basic sentiment without preprocessing for that comment in a new column with the name of sentiment'''
-    #this wasn't working so rn i will try making a new list that hass all the sentiments
+    #this wasn't working so rn i will try making a new list that has all the sentiments
+    
+    comment_column = data["Comment"]
+    comments = []
+    for i in comment_column:    
+        comments.append(i)
+    sentiments = []
+    for j in comments:
+        comment_sentiment = TextBlob(j).sentiment
+
+
+
+    '''
     sentiments = []
     error_count = 0
     for index, row in tqdm(data.iterrows(), ncols=100):
@@ -22,8 +34,19 @@ def plain_sentiment(data):
             error_count += 1
     print (error_count)
     return sentiments 
-sentiments = plain_sentiment(dataset)
-print (sentiments)
+    '''
+#sentiments = plain_sentiment(dataset)
+#print (sentiments)
+comment_column = dataset["Comment"]
+comments = []
+#print (len(comment_column))
+for i in comment_column:
+    comments.append(i)
+sentiment = TextBlob(comments[64523]).sentiment
+temp = []
+temp.append(sentiment.polarity)
+temp.append(sentiment.subjectivity)
+print (temp)
 
 
 
