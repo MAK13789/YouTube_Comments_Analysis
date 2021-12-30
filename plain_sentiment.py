e6de3bfdbab85f1331a6c872798171b69c425c85
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-12-30 18:34:08 -0500

    :eyes:

--- plain_sentiment.py
+++ plain_sentiment.py
@@ -3,7 +3,7 @@ this file will directly do sentiment analysis on the dataset, without any prepro
 '''
 from textblob import TextBlob
 import pandas as pd
-from tqdm import tqdm
+#from tqdm import tqdm
 dataset = pd.read_csv('dataset.csv')
 def plain_sentiment(data):
     '''takes in a dataset in the form of dataset.csv and for each comment adds the basic sentiment without preprocessing for that comment in a new column with the name of sentiment'''
@@ -15,31 +15,21 @@ def plain_sentiment(data):
         comments.append(i)
     sentiments = []
     for j in comments:
-        comment_sentiment = TextBlob(j).sentiment
-
-
-
-    '''
-    sentiments = []
-    error_count = 0
-    for index, row in tqdm(data.iterrows(), ncols=100):
         try:
-            comment = data.at[row, "Comment"]
-            sentiment = TextBlob(comment).sentiment
+            comment_sentiment = TextBlob(j).sentiment    
             temp = []
-            temp.append(sentiment.polarity)
-            temp.append(sentiment.subjectivity)
-            sentiments.append(temp)  
+            temp.append(comment_sentiment.polarity)
+            temp.append(comment_sentiment.subjectivity)
+            sentiments.append(temp)
         except:
-            error_count += 1
-    print (error_count)
-    return sentiments 
-    '''
-#sentiments = plain_sentiment(dataset)
-#print (sentiments)
+            sentiments.append([0, 0])
+    '''add sentiments as a column to the df'''
+    return sentiments
+sentiments = plain_sentiment(dataset)
+print (sentiments)
+'''
 comment_column = dataset["Comment"]
 comments = []
-#print (len(comment_column))
 for i in comment_column:
     comments.append(i)
 sentiment = TextBlob(comments[64523]).sentiment
@@ -47,11 +37,4 @@ temp = []
 temp.append(sentiment.polarity)
 temp.append(sentiment.subjectivity)
 print (temp)
-
-
-
 '''
-returns empty list and error count which is the same as the num of iterations......
-doing sth wrong then so gotta fix this
-'''
-
