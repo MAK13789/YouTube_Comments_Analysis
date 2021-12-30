09ff2fadfd5587b8b48a3874c52fc090c8d7444d
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-12-30 18:47:38 -0500

    :flushed:

--- plain_sentiment.py
+++ plain_sentiment.py
@@ -3,12 +3,9 @@ this file will directly do sentiment analysis on the dataset, without any prepro
 '''
 from textblob import TextBlob
 import pandas as pd
-#from tqdm import tqdm
 dataset = pd.read_csv('dataset.csv')
 def plain_sentiment(data):
     '''takes in a dataset in the form of dataset.csv and for each comment adds the basic sentiment without preprocessing for that comment in a new column with the name of sentiment'''
-    #this wasn't working so rn i will try making a new list that has all the sentiments
-    
     comment_column = data["Comment"]
     comments = []
     for i in comment_column:    
@@ -23,18 +20,7 @@ def plain_sentiment(data):
             sentiments.append(temp)
         except:
             sentiments.append([0, 0])
-    '''add sentiments as a column to the df'''
-    return sentiments
-sentiments = plain_sentiment(dataset)
-print (sentiments)
-'''
-comment_column = dataset["Comment"]
-comments = []
-for i in comment_column:
-    comments.append(i)
-sentiment = TextBlob(comments[64523]).sentiment
-temp = []
-temp.append(sentiment.polarity)
-temp.append(sentiment.subjectivity)
-print (temp)
-'''
+    data["Sentiment"] = sentiments
+    return data
+updated_dataset = plain_sentiment(dataset)
+print (updated_dataset)
