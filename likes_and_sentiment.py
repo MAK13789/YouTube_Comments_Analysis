0b7da0df68c433e601ba5e536b787b261167f077
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2022-01-19 10:57:32 -0500

    :eye:

--- likes_and_sentiment.py
+++ likes_and_sentiment.py
@@ -1,3 +1,17 @@
 '''
 this file will investigate the relationship between the number of likes a comment has and its sentiment (polarity and subjectivity score)
 '''
+import pandas as pd
+dataset = pd.read_csv('plain_sentiment.csv')
+likes_column = dataset["Likes"]
+likes_complete = []
+for i in likes_column:
+    likes_complete.append(i)
+sentiment_column = dataset["Sentiment"]
+sentiment_complete = []
+for j in sentiment_column:
+    sentiment_complete.append(j)
+print (likes_complete)
+print (sentiment_complete)
+print (len(likes_complete))
+print (len(sentiment_complete)
